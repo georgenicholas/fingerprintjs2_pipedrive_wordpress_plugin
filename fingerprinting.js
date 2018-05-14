41eefde64e2a4a32804df676941032513fe31982
George Nicholas <george@dringo.io>
2018-05-14 12:50:38 -0700

    it is sending the fingerprint via ajax to php now

--- fingerprinting.js
+++ fingerprinting.js
@@ -1,16 +1,13 @@
 jQuery(document).ready(function($) {
-
     // We'll pass this variable to the PHP function example_ajax_request
     new Fingerprint2().get(function(result, components) {
 
-
-
     // This does the ajax request
       $.ajax({
-          url: example_ajax_obj.ajaxurl, // or example_ajax_obj.ajaxurl if using on frontend
+          url: fingerprinting_ajax_obj.ajaxurl, // or example_ajax_obj.ajaxurl if using on frontend
           data: {
-              'action': 'example_ajax_request',
-              'fruit' : result
+              'action': 'fingerprinting_ajax_request',
+              'fingerprint' : result
           },
           success:function(data) {
               // This outputs the result of the ajax request
